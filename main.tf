287a9dd1afc0a37655480ce815eb8d4947a089df
Ranjita0707 <ranjitapattnaik307@gmail.com>
2025-04-16 19:48:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-00a929b66ed6e0de6"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "terr-key"
+vpc_security_group_ids = ["sg-0233e8710636d4236"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
